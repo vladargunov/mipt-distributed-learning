568d5053d4d557abc43c158153140a2f0b48fd0a
v-argunov <argunovvlad5@gmail.com>
2022-12-01 01:34:12 +0300

    corrected run_distributed_train

--- data-parallel/processing/run_distributed_train.py
+++ data-parallel/processing/run_distributed_train.py
@@ -2,8 +2,9 @@ import torch
 
 
 if __name__ == '__main__':
+    world_size = torch.distributed.init_process_group
     torch.distributed.init_process_group(
         "nccl",
         rank=0,
-        world_size=torch.cuda.device_count()
+        world_size=world_size
     )
